ee46b09c489551ce32d33ea5538bcd18506ace42
Prabhjot Singh <prabhjotsingh394@gmail.com>
2023-06-19 11:26:06 +0530

    Updated logic for Moderation Service

--- index.js
+++ index.js
@@ -20,27 +20,28 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[req.params.id] = comments;
 
   await axios.post('http://localhost:4005/events', {
-        type: 'CommentCreated',
-        data: {
-            id: commentId,
-            content,
-            postId: req.params.id
-        }
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: 'pending'
+    }
   });
 
   res.status(201).send(comments);
 });
 
 app.post('/events', (req, res) => {
-  console.log('Received Event', req.body.type);
+  console.log('Event Received:', req.body.type);
 
   res.send({});
-})
+});
 
 app.listen(4001, () => {
   console.log('Listening on 4001');
